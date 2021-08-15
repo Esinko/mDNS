6f3d337e86543f6b3a4464d840e36816067cb7cf
Esinko <34211401+Esinko@users.noreply.github.com>
2021-08-15 14:32:11 +0300

    chore: Cleanup

--- test.js
+++
@@ -1,3 +0,0 @@
-const resolve = require("./index.js")
-
-resolve("38aac546-f8af-4649-8e63-d1806df2a488.local")
--- test3.js
+++
@@ -1,7 +0,0 @@
-const dns = require("dns")
-
-dns.setServers(["224.0.0.251:5353"])
-
-dns.resolve("38aac546-f8af-4649-8e63-d1806df2a488.local", "A", (err, address) => {
-    console.log(address)
-})
